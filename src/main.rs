8a52ac50e35d1b5d3692b7de697dc148e5a9e6b9
A. B. Sørensen <anders-soerensen@hotmail.com>
2021-12-12 17:02:07 +0100

    Ready for first rays

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate minifb;
 use minifb::{Key, ScaleMode, Window, WindowOptions};
 
+extern crate raytracing_in_rust;
+use raytracing_in_rust::geometry::vector3::*;
+
 const IMAGE_WIDTH: i64 = 256;
 const IMAGE_HEIGHT: i64 = 256;
 const IMAGE_COLOR_MODE: i64 = 3;
@@ -10,21 +13,14 @@ fn main() {
     let mut image_buffer: Vec<f64> = vec![0.0; (IMAGE_WIDTH * IMAGE_HEIGHT * IMAGE_COLOR_MODE) as usize];
 
     for row_index in 0..IMAGE_HEIGHT {
+        println!("Tracing line {} of {}", row_index, IMAGE_HEIGHT);
         for column_index in 0..IMAGE_WIDTH {
-            let r = column_index as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = row_index as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
+            let color = Vector3{0: column_index as f64 / (IMAGE_WIDTH - 1) as f64, 1: row_index as f64 / (IMAGE_HEIGHT - 1) as f64, 2: 0.25};
+            let buffer_offset: usize = ((IMAGE_HEIGHT - 1 - row_index) * IMAGE_WIDTH * IMAGE_COLOR_MODE + column_index * IMAGE_COLOR_MODE + 0) as usize;
 
-            image_buffer[(row_index * IMAGE_WIDTH * IMAGE_COLOR_MODE + column_index * IMAGE_COLOR_MODE + 0) as usize] = (255.999 * r) as f64;
-            image_buffer[(row_index * IMAGE_WIDTH * IMAGE_COLOR_MODE + column_index * IMAGE_COLOR_MODE + 1) as usize] = (255.999 * g) as f64;
-            image_buffer[(row_index * IMAGE_WIDTH * IMAGE_COLOR_MODE + column_index * IMAGE_COLOR_MODE + 2) as usize] = (255.999 * b) as f64;
+            color.color_to_output(&mut image_buffer, buffer_offset);
         }
     }
-
-
-
-
-
 
     let window_buffer: Vec<u32> = image_buffer
         .chunks(3)
@@ -43,13 +39,15 @@ fn main() {
     )
     .expect("Unable to open Window");
 
+
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
-            .update_with_buffer(
-                &window_buffer,
-                IMAGE_WIDTH as usize,
-                IMAGE_HEIGHT as usize,
-            )
-            .unwrap();
+        .update_with_buffer(
+            &window_buffer,
+            IMAGE_WIDTH as usize,
+            IMAGE_HEIGHT as usize,
+        )
+        .unwrap();
     }
 }
