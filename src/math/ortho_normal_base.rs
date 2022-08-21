43195e10cd08dc1befb25bc12b2a14479a8c18b2
A. B. Sørensen <anders-soerensen@hotmail.com>
2022-08-21 19:36:17 +0200

    Revert "Implement Frisvad cross product method for better performance. 1 second < performance improvement on benchmark"
    This reverts commit 9e269b35c51d791889b3dd49a7df4d82ebc2e4d9.

--- src/math/ortho_normal_base.rs
+++ src/math/ortho_normal_base.rs
@@ -15,48 +15,25 @@ pub struct OrthoNormalBase {
 
 impl OrthoNormalBase {
     pub fn build_from_w(n: &Vector3<f32>) -> OrthoNormalBase {
-        // Implementation of Frisvad based on onb code from https://people.compute.dtu.dk/jerf/code/
-        if(n.z < -0.999999999) {
-            let v: Vector3<f32> = Vector3::<f32>::new( 0.0, -1.0, 0.0);
-            let u: Vector3<f32> = Vector3::<f32>::new(-1.0,  0.0, 0.0);
-            return OrthoNormalBase { u, v, w: n.clone() };
-        }
+        let w: Vector3<f32> = n.normalize();
+        let a: Vector3<f32> = if 0.9 < w.x.abs() { Vector3::<f32>::new(0.0, 1.0, 0.0 ) } else { Vector3::<f32>::new(1.0, 0.0, 0.0 )};
+        let mut v: Vector3<f32> = Vector3::cross(&w, &a);
+        v.normalize_mut();
+        let u: Vector3<f32> = Vector3::cross(&w, &v);
 
-        let a: f32 = 1.0/(1.0 + n.z);
-        let b: f32 = -n.x*n.y*a;
-        let v: Vector3<f32> = Vector3::<f32>::new(1.0 - n.x*n.x*a, b, -n.x);
-        let u: Vector3<f32> = Vector3::<f32>::new(b, 1.0 - n.y*n.y*a, -n.y);
-        return OrthoNormalBase { u, v, w: n.clone() };
-
+        OrthoNormalBase { u, v, w }
     }
 
     // Update this to not require any more new structs. Maybe do a cross and normalized with a supplied result argument
     // And make the two Vector3's static
     #[inline]
     pub fn update(&mut self, n: Vector3<f32>) {
-        // Implementation of Frisvad based on onb code from https://people.compute.dtu.dk/jerf/code/
         self.w = n;
-        if self.w.z < -0.999999999 {
-            self.v.x = 0.0;
-            self.v.y = -1.0;
-            self.v.z = 0.0;
-
-            self.u.x = -1.0;
-            self.u.y = 0.0;
-            self.u.z = 0.0;
-            return;
-        }
-
-        let a: f32 = 1.0/(1.0 + n.z);
-        let b: f32 = -n.x*n.y*a;
-
-        self.v.x = 1.0 - n.x*n.x*a;
-        self.v.y = b;
-        self.v.z = -n.x;
-        
-        self.u.x = b;
-        self.u.y = 1.0 - n.y*n.y*a;
-        self.u.z = -n.y;
+        self.w.normalize_mut();
+        let a: &Vector3<f32> = if 0.9 < self.w.x.abs() { &Y_VECTOR } else { &X_VECTOR };
+        self.v = Vector3::cross(&self.w, &a);
+        self.v.normalize_mut();
+        self.u = Vector3::cross(&self.w, &self.v);
     }
 
     pub fn local_vector(&self, a: &Vector3<f32>) -> Vector3<f32> {
