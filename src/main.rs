478dd1d5eebe65191eb02aef6958cb0917ef79f1
A. B. Sørensen <anders-soerensen@hotmail.com>
2022-08-21 11:53:57 +0200

    Redo book3 image

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
-    // raytracing_in_rust::render("C:\\Programming\\raytracing_in_rust\\render_config\\benchmark.render_config");
     raytracing_in_rust::render("render_config/benchmark.toml");
 }
